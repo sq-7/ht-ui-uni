aac6f4d075fdddf28734aebc5cb64a232ab76377
huangweijian <huangweijian@haotaisoft.com>
2025-06-04 15:09:23 +0800

    fix: 🐛 remove comment

--- scripts/release.ts
+++ scripts/release.ts
@@ -102,7 +102,5 @@ inquirer
   .catch((error: any) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
     }
   })
